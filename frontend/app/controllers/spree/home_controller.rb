6668f5c610a72fec8ce8fe82a13fd4c6a80e88c2
Tom Scott <tscott@weblinc.com>
2015-04-04 21:16:15 -0400

    Fix duplicate products being shown on index

--- frontend/app/controllers/spree/home_controller.rb
+++ frontend/app/controllers/spree/home_controller.rb
@@ -5,7 +5,7 @@ module Spree
 
     def index
       @searcher = build_searcher(params.merge(include_images: true))
-      @products = @searcher.retrieve_products
+      @products = Spree::Product.all
       @taxonomies = Spree::Taxonomy.includes(root: :children)
     end
   end
